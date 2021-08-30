29422852bc8b21e295692bc827b969908419a1f2
Behzad Eslami Tehrani <betehrani@gmail.com>
2021-08-30 15:45:40 +0430

    Project initializing

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuantQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
